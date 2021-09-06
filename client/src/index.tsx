ad9a3002e16cc99ac0a02851e36fae837f6e5b01
Artem Derenok <derenokartem95@gmail.com>
2021-09-06 15:41:34 +0300

    feat: add material ui

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './app';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto';
 
 ReactDOM.render(
   <React.StrictMode>
