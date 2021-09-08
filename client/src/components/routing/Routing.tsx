8ed1998f9d342db03319312c1771085ddcc6ef71
Artem Derenok <derenokartem95@gmail.com>
2021-09-08 15:20:09 +0300

    feat: add routing

---
+++ client/src/components/routing/Routing.tsx
@@ -0,0 +1,21 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Routes } from './routes';
+import { Main } from '../MainPage/main';
+import styles from './Routing.module.scss';
+
+export const Routing: React.FC = () => {
+  return (
+    <Box mb="1rem" className={styles.container}>
+      <Router>
+        <Switch>
+          {Routes.map(elem => (
+            <Route key={elem.key} exact path={`${elem.path}`} component={elem.component} />
+          ))}
+          <Route component={Main} />
+        </Switch>
+      </Router>
+    </Box>
+  );
+};
