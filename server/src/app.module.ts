10657b64805f982118d041c24b89ffffc47b75ca
Anatoli Rubankou <catok1992@gmail.com>
2021-09-10 10:12:58 +0300

    feat: setup mongo and mongoose, implement game module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GameModule } from './game/game.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.mpoqa.mongodb.net/Games?retryWrites=true&w=majority`,
+    ),
+    GameModule,
+  ],
+})
 export class AppModule {}
