5fe9c2160c5ffea3567de4b55adaa00a73a043fb
Artem <derenokartem95@gmail.com>
2021-09-23 14:47:40 +0300

    refactor: move styles import to the bottom

--- client/src/components/GamePage/GamePage.tsx
+++ client/src/components/GamePage/GamePage.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { useTypeSelector } from '../../hooks/useTypeSelector';
 import { Roles } from '../../core/types/roleType';
 import { CardField } from './CardField/CardField';
-import styles from './GamePage.module.scss';
 import { IssueGameSection } from './IssueGameSection/IssueGameSection';
 import { ScramMasterGameSection } from './ScramMasterGameSection/ScramMasterGameSection';
 import { ProgressSection } from './ProgressSection/ProgressSection';
+import styles from './GamePage.module.scss';
 
 export const GamePage: React.FC = () => {
   const { currentUser } = useTypeSelector(state => state.currentUser);
