efca6aa0b3842b49bd56eff65433902775d4a8ba
GlukoLyoFF <glukolyoff@gmail.com>
2021-09-28 18:17:21 +0100

    fix: fix title value in EditHeading

--- client/src/core/components/editHeading/EditHeading.tsx
+++ client/src/core/components/editHeading/EditHeading.tsx
@@ -10,7 +10,6 @@ export const EditHeading: React.FC = () => {
   const { gameInfo } = useTypeSelector(state => state.gameInfo);
   const dispatch = useDispatch();
   const { currentUser } = useTypeSelector(state => state.currentUser);
-  const [getInputValue, setInputValue] = useState('');
   const [isInputDisabledValue, setInputDsiabledValue] = useState(true);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -31,7 +30,7 @@ export const EditHeading: React.FC = () => {
       <input
         className={styles.input}
         ref={inputRef}
-        value={getInputValue}
+        value={gameInfo.title}
         type="text"
         onChange={handleChangeTitle}
         disabled={isInputDisabledValue}
