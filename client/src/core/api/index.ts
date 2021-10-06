fb6cbeee644287091179faa87a41e13af9bfe32a
Anatoli Rubankou <catok1992@gmail.com>
2021-10-06 22:10:32 +0300

    fix: fix path to server

--- client/src/core/api/index.ts
+++ client/src/core/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://carabaz.herokuapp.com/api',
+  baseURL: 'https://carabaz.herokuapp.com/api',
   timeout: 5000,
 });
 
